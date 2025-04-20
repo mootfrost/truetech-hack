78bd6c21a4efcdc8ed8142b9d7df3129c1cc6fd6
Vnk2 <volodinnikolaj641@gmail.com>
2025-04-20 17:51:08 +0300

    Initial

--- app/agents/IntentAgent.py
+++ app/agents/IntentAgent.py
@@ -16,8 +16,8 @@ class IntentAgent(BaseAgent):
     @staticmethod
     async def get_intent(text: str) -> str:
         messages = [
-            SystemMessage(content='Ты четко определяешь только intent запроса сначала на русском а потом на английском и делаешь это не более чем за 4 слова для каждого'),
-            HumanMessage(content=f'определи интент: {text}'),
+            SystemMessage(content='Ты четко определяешь только intent запроса на русском и делаешь это не более чем за 5 слов. Никогда не пиши ничего кроме intent. Intet - это предмет того, что хочет пользователь или с чем возникла проблема'),
+            HumanMessage(content=f'определи интент: {text}.'),
         ]
         response = await chat_model.apredict_messages(messages)
         return response.content
@@ -37,20 +37,33 @@ class IntentAgent(BaseAgent):
         user_data = users.get(user_id, 'Нет данных')
         messages = [
             SystemMessage(content='ты ищешь зацепки о финансовом положении пользователя и его лояльности'),
-            HumanMessage(content=f'Проанализируй информацию и определи кейсы {user_data}'),
+            HumanMessage(content=f'Проанализируй информацию и определи кейсы {user_data}. Напиши только самый правдоподобный'),
         ]
         response = await chat_model.apredict_messages(messages)
         return response.content
 
+
+    @staticmethod
+    async def summ_ask(intent:str, entitiesstr:str, user_info:str) -> str:
+        message = [
+            SystemMessage(content='Проанализировав все свойствыа ты даешь краткую точную информацию зачем пользователь написал в поддержку на основании данных про него исключительно итоговую оценку'),
+            HumanMessage(content=f'Проанализируй {intent}, {entitiesstr}, {user_info} '),]
+
+        response = await chat_model.apredict_messages(message)
+        return response.content
+
+
     async def run(self, query: str, context: str = '') -> str:
         user_id = context or 'default'
         preprocessed = self.preprocess(query)
-        intent = await self.get_intent(preprocessed)
-        entities = await self.extract_entities(intent, preprocessed)
         user_info = await self.analyze_user(user_id)
+        intent = await self.get_intent(preprocessed + 'То, что мы знаем о пользователе, это может помочь:' + user_info)
+        entities = await self.extract_entities(intent, preprocessed)
 
         print(f'[Intent]: {intent}')
         print(f'[Entities]: {entities}')
         print(f'[User Info]: {user_info}')
 
-        return user_info
+        final_answer = await self.summ_ask(intent, entities, user_info)
+        print(final_answer)
+        return final_answer
