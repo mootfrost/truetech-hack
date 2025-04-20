523ab0d8ff67ca0703da635307746c3ea84894ad
Vnk2 <volodinnikolaj641@gmail.com>
2025-04-20 15:47:14 +0300

    Add emo agent

--- app/agents/__init__.py
+++ app/agents/__init__.py
@@ -1,3 +1,4 @@
 from app.agents.RagAgent import RagAgent
 from app.agents.IntentAgent import IntentAgent
 from app.agents.BaseAgent import BaseAgent
+from app.agents.EmotionAgent import EmotionAgent
--- app/routes/emotion_agent.py
+++ app/routes/emotion_agent.py
@@ -1,37 +1,11 @@
 from fastapi import APIRouter
-from app.config import config
-import requests
-import re
+from app.agents import EmotionAgent
+
+router = APIRouter(prefix='/emot')
+agent = EmotionAgent()
 
 
-def preprocess(text):
-    text = text.lower()
-    text = re.sub(r"[^\w\s]", "", text)      # удаление пунктуации
-    text = text.strip()
-    return text
-
-
-def classify_with_model(text):
-    payload = {
-        "model": 'mws-gpt-alpha',
-        "messages": [{"role": "user", "content": f"Определи эмоции клиента: '{text}'"}],
-        "temperature": 0
-    }
-    return  payload
-
-headers = {
-    'Authorization': 'Bearer ' + config.openai_token
-}
-
-
-router = APIRouter(prefix='/emo')
-@router.get('/request')
-async def remo(text: str):
-    resp = requests.post('https://api.gpt.mws.ru/v1/chat/completions', headers=headers, json=classify_with_model(text))
-    print(resp.json()["choices"][0]["message"]["content"])
-    return {'message': 'em agent'}
-
-
-
-
-__all__ = ['router']
+@router.get('/query-agent')
+async def request(text: str, id: str):
+    result = await agent.run(query=text, context=id)
+    return {'message': result}
