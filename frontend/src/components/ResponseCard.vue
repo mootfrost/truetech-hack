2ef0ac65b430eab265190cb1256dbd1938b921e7
Mootfrost777 <andrey13102007@gmail.com>
2025-04-21 22:44:03 +0300

    Fix 1003

--- frontend/src/components/ResponseCard.vue
+++ frontend/src/components/ResponseCard.vue
@@ -8,9 +8,9 @@ defineProps<{ suggestion: Suggestion }>()
 <template>
   <span><strong>Причина обращения: </strong>{{suggestion.intent}}</span><br>
   <span><strong>Эмоция: </strong>{{suggestion.emotion}} ({{suggestion.emotion_force}})</span><br>
-  <span><strong>Рекомендуемое решение: </strong>{{suggestion.answer}}</span><br><br>
+  <span><strong>Рекомендуемое решение: </strong>{{suggestion.answer}}</span><br><br><br><br><br><br>
 
-  <p><strong>Анализ полученного решения: </strong>{{suggestion.qa}}</p><br><br>
+  <p><strong>Возможный способ улучшения решения: </strong>{{suggestion.qa}}</p><br><br>
 </template>
 
 <style scoped>
